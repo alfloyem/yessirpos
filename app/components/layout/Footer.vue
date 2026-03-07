00e8b6650206c191bdea5035a9a4bf00c7ffc43c
Musa Rzayev <musarzayev.65@gmail.com>
2026-03-07 16:00:19 +0400

    footer

--- app/components/layout/Footer.vue
+++ app/components/layout/Footer.vue
@@ -1,20 +1,13 @@
 <script setup>
-import { useI18n } from '#i18n'
-const { t } = useI18n()
-const date = new Date().getFullYear();
+const currentYear = new Date().getFullYear()
 </script>
 
 <template>
-  <footer class="p-8 text-center border-t border-[var(--border-app)] mt-auto">
-    <div class="flex flex-col items-center gap-4">
-      <p class="text-[8px] text-[var(--text-app)] opacity-40 tracking-[.5em] font-black">
-        &copy; {{ date }} YESSIRPOS
+  <footer class="p-4 text-center border-t border-[var(--border-app)] mt-auto font-sans">
+    <div class="flex items-center justify-center">
+      <p class="text-xs text-[var(--text-app)] opacity-60 font-medium">
+        &copy; {{ currentYear }} Powered by <span class="font-bold text-[var(--text-primary)]">YESSIRPOS</span>. Bütün Hüquqlar Qorunur.
       </p>
-      
-      <div class="flex gap-8 opacity-40">
-        <a href="#" class="text-[7px] text-[var(--text-app)] tracking-widest font-black hover:opacity-100 hover:text-[var(--text-primary)] transition-all">{{ t('footer.terms') }}</a>
-        <a href="#" class="text-[7px] text-[var(--text-app)] tracking-widest font-black hover:opacity-100 hover:text-[var(--text-primary)] transition-all">{{ t('footer.privacy') }}</a>
-      </div>
     </div>
   </footer>
 </template>
