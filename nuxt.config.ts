50878b24fb52e1b2ce814f68565e18459eb8ba9a
name name <alfloyem@gmail.com>
2026-04-20 01:39:27 +0400

    chore: hardcode public Firebase client config, keep only secrets in env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,11 +13,11 @@ export default defineNuxtConfig({
       clientId: process.env.CLIENT_ID ?? 'bakustreet',
       apiBaseUrl: process.env.NUXT_PUBLIC_API_URL ?? '',
       firebase: {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
+        apiKey: 'AIzaSyDStysEJmuogwBBhqEDDp5kJQdxR6WKI60',
+        authDomain: 'yessirpos.firebaseapp.com',
+        projectId: 'yessirpos',
+        messagingSenderId: '1077686504612',
+        appId: '1:1077686504612:web:49985eb7ad488af318aa31',
         vapidKey: process.env.FIREBASE_VAPID_KEY,
       }
     },
